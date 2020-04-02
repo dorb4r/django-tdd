fbdd4f695307118894bbb61e88ebbfca1d84772c
Dor Bar <dor.b4r@gmail.com>
2020-04-02 22:33:20 +0300

    chore: Add flake8 to lint the codebase - fix the liter errors

--- app/movies/urls.py
+++ app/movies/urls.py
@@ -2,7 +2,6 @@
 
 """
 
-from django.urls import path
 from rest_framework import routers
 
 from movies import views
@@ -11,7 +10,3 @@ router = routers.SimpleRouter()
 router.register(r"movies", views.MoviesViewSet)
 
 urlpatterns = router.urls
-# [
-#     path('movies/', views.MovieList.as_view(), name="create-movie"),
-#     path('movies/<int:pk>', views.MovieDetail.as_view(), name="retrieve-movie"),
-# ]
